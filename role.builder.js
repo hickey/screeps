16b806641b6d91ff5565484911c4af813ad88fe0
Gerard Hickey <Gerard.Hickey@smartsheet.com>
2017-11-06 01:13:52 -0800

    Update of roleBuilder to FSM

--- role.builder.js
+++ role.builder.js
@@ -1,39 +1,181 @@
+
+/** missions **/
+var M_unassigned = 0;
+var M_harvest    = 1;
+var M_build      = 2;
+var M_upgrade    = 3;
+var M_mine       = 4;
+
+
+/** tasks **/
+var T_unassigned     = 0;
+var T_discharge      = 2;
+var T_build          = 3;
+var T_upgrade        = 4;
+var T_recharge       = 5;
+var T_move           = 7;
+
+
 var roleBuilder = {
+
 
     /** @param {Creep} creep **/
     run: function(creep) {
-        //console.log("roleBuilder.run("+creep+")");
-	    if(creep.memory.building && creep.carry.energy == 0) {
-            creep.memory.building = false;
-            creep.say('🔄 harvest');
-	    }
-	    if(!creep.memory.building && creep.carry.energy == creep.carryCapacity) {
-	        creep.memory.building = true;
-	        creep.say('🚧 build');
-	    }
-
-	    if(creep.memory.building) {
-	        var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-	        //console.log("build targets="+targets);
-            if(targets.length) {
-                if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+        //console.log("roleHarvester.run()");
+        
+        if (!creep.memory.task) {
+            creep.memory.task = T_unassigned;
+        }
+        var goal = Game.getObjectById(creep.memory.goal);
+        var target = Game.getObjectById(creep.memory.target);
+        
+        //console.log("harvester task=", creep.memory.task);
+        switch (creep.memory.task) {
+            
+            case T_unassigned:
+                let source = locateEnergySource(creep);
+                if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
+                    nextTask(creep, T_move, {setGoal: source.id});
                 }
-            } else {
-                var target = creep.room.find(FIND_STRUCTURES, {filter: (structure) => {
-                            return (structure.structureType == STRUCTURE_CONTAINER)}});
-                console.log("target = ", target);
-                creep.moveTo(target, {visualizePathStyle: {stroke: '#00ff00'}});
-            }
-	    }
-	    else {
-	        var sources = creep.room.find(FIND_SOURCES_ACTIVE);
-	        //var instance = Math.round(Math.random() * sources.length);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
-            }
-	    }
+                break;
+                
+                
+            case T_move:
+                var res = creep.moveTo(goal, {visualizePathStyle: {stroke: '#ffff00'}});
+                // console.log("res = ", res);
+                switch(res) {
+                    case OK:
+                        var distance = creep.pos.getRangeTo(goal);
+                        if (distance == 1) {
+                            //console.log("reached goal, reassing to target");
+                            // reached goal, move to next state
+                            creep.memory.target = creep.memory.goal;
+                            creep.memory.goal = null;
+                            var target = Game.getObjectById(creep.memory.target);
+                
+                            var targets = creep.room.lookAt(target);
+                            switch (targets[0].type) {
+                                case LOOK_SOURCES:
+                                    console.log("found energy source");
+                                    creep.memory.task = T_recharge;
+                                    creep.harvest(target);
+                                    break;
+                                case LOOK_CONSTRUCTION_SITES:
+                                    console.log("found build structure");
+                                    creep.memory.task = T_build;
+                                    creep.build(target);
+                                    break;
+                                default:
+                                    console.log("found nothing");
+                                    console.log(JSON.stringify(targets));
+                                    break;
+                            } 
+                        }
+                        break;
+                    case ERR_TIRED:
+                    case ERR_BUSY:
+                        break;
+                    case ERR_NO_BODYPART:
+                        creep.suicide();
+                        return;
+                        break;
+                    case ERR_INVALID_TARGET:
+                        nextTask(creep, T_unassigned, {setGoal: null});
+                        creep.say("ARG!");
+                        break;
+                }
+                break;
+                
+            
+            case T_recharge:
+                if (creep.carry.energy < creep.carryCapacity) {
+                    var res = creep.harvest(target);
+                    if (res == ERR_NOT_ENOUGH_RESOURCES) {
+                        nextTask(creep, T_unassigned, {setGoal: null});
+                    }
+                } else {
+                    // finished recharging
+                    var target = locateBuildTarget(creep);
+                    if (target) {
+                        nextTask(creep, T_move, {setGoal: target.id});
+                    }
+                    creep.say("🚧 build");
+                }
+                break;
+                
+    
+            case T_build:
+                if (creep.carry.energy > 0) {
+                    let res = creep.build(target);
+                    if (res == ERR_NOT_IN_RANGE || res == ERR_INVALID_TARGET) {
+                        let nextTarget = locateBuildTarget(creep);
+                        if (nextTarget) {
+                            nextTask(creep, T_move, {setGoal: nextTarget.id});
+                        }
+                    }
+                } else {
+                    // finished transfer of energy
+                    nextTask(creep, T_unassigned, {setGoal: null});
+                    creep.say("🔄 harvest");
+                }
+                break;
+                
+                
+            
+            
+        }
+        
 	}
 };
+
+
+function nextTask(creep, newTask, options) {
+    creep.memory.task = newTask;
+    if (options.setGoal) {
+        creep.memory.target = creep.memory.goal;
+        creep.memory.goal = options.setGoal;
+    }
+    
+    if (options.setAcheivement) {
+        creep.memory.acheivement = options.setAcheivement;
+    }
+    
+    if (options.clearAcheivement) {
+        delete creep.memory.acheivement;
+    }
+}
+
+
+function locateEnergySource(creep) {
+    let sources = creep.room.find(FIND_SOURCES_ACTIVE);
+    let instance = Math.round(Math.random() * sources.length);
+    return sources[instance];
+};
+
+
+function locateBuildTarget(creep) {
+    
+    var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+	//console.log("build targets="+targets);
+    if(targets) {
+        return targets[Math.round(Math.random() * targets.length)];
+    } else {
+        return null;
+    }
+}
+
+function locateDeliveryTarget(creep) {
+    var targets = creep.room.find(FIND_STRUCTURES, {
+                    filter: (structure) => {
+                            return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
+                                    structure.energy < structure.energyCapacity;} });
+    if (targets) {   
+        return targets[Math.round(Math.random() * targets.length)];
+    } else {
+        return null;
+    }
+}
+
+
 
 module.exports = roleBuilder;
